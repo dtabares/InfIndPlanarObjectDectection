9a02d504136873099a6cf6440f4aec8ec15e6f52
Diego Tabares <tabares@ar.ibm.com>
2017-05-13 13:39:15 -0300

    first preview of the camera

--- app/src/main/java/ar/edu/untref/infindplanarobjectdectection/MainActivity.java
+++ app/src/main/java/ar/edu/untref/infindplanarobjectdectection/MainActivity.java
@@ -1,13 +1,111 @@
 package ar.edu.untref.infindplanarobjectdectection;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG="MainActivity";
+    private int MY_PERMISSIONS_REQUEST_CAMERA;
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this){
+        @Override
+        public void onManagerConnected(int status){
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv sucessfully loaded");
+        }
+        else{
+            Log.d(TAG, "Opencv not loaded");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "tiene permisos");
+
+        } else {
+            Log.v(TAG, "no tiene permisos");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null){
+            javaCameraView.disableView();
+        }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv sucessfully loaded");
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else{
+            Log.d(TAG, "Opencv not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9,this,baseLoaderCallback);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height,width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
     }
 }
